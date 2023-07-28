ac3caea429422d6d84c18f3aaeba49d9c7593d02
jalopbauer <jorge.lopezbauer@ing.austral.edu.ar>
2023-07-28 01:19:37 -0300

    F fromDoubleToMoney

--- lib/src/main/kotlin/util/money/Money.kt
+++ lib/src/main/kotlin/util/money/Money.kt
@@ -8,4 +8,28 @@ data class Money(private val whole: ULong, val decimal: UByte) {
         val decimalRem = wholeRem.rem(n)
         return Pair(Money(wholeDiv, decimalDiv), decimalRem)
     }
+
+    fun fromDouble(double: Double): Money {
+        val doubleString = double.toString()
+        val (valuesBeforeDot, valuesAfterDot) = doubleString
+            .fold(Triple("", "", false)) {
+                    (valuesBeforeDot, valuesAfterDot, hasDotAppeared), c ->
+                when (c) {
+                    '.' -> Triple(valuesBeforeDot, valuesAfterDot, true)
+                    else ->
+                        if (hasDotAppeared) {
+                            if (valuesBeforeDot.length >= 2) {
+                                Triple(valuesBeforeDot, valuesAfterDot, hasDotAppeared)
+                            } else {
+                                Triple(valuesBeforeDot, valuesAfterDot + c, hasDotAppeared)
+
+                            }
+                        } else{
+                            Triple(valuesBeforeDot + c, valuesAfterDot, hasDotAppeared)
+                        }
+                }
+
+            }
+        return Money(valuesBeforeDot.toULong(), valuesAfterDot.toUByte())
+    }
 }
